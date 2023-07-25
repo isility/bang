0c2e74a9d77395edee54b5bfa8558d3c507cdcaa
isility <voice_beggar@nate.com>
2023-07-25 14:11:58 +0900

    payment commit

--- src/main/java/kr/co/jhta/bang/finalproject/Control/PaymentController.java
+++ src/main/java/kr/co/jhta/bang/finalproject/Control/PaymentController.java
@@ -1,17 +1,42 @@
 package kr.co.jhta.bang.finalproject.Control;
 
+import kr.co.jhta.bang.finalproject.service.KakaoPay;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 @RequestMapping("/payment")
+@Slf4j
 public class PaymentController {
-    @GetMapping("/main")
-    public String list(Model model) {
 
-        return "payment/main";
+    @Setter(onMethod_ = @Autowired)
+    private KakaoPay kakaopay;
+
+
+    @GetMapping("/kakaoPay")
+    public void kakaoPayGet() {
+        log.info("카카페 래디1");
     }
+
+    @PostMapping("/kakaoPay")
+    public String kakaoPay() {
+        log.info("카카페 래디2");
+        return "redirect:" + kakaopay.kakaoPayReady();
+
+    }
+
+    @GetMapping("/kakaoPaySuccess")
+    public void kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+        log.info("카카페 성공");
+        model.addAttribute("info", kakaopay.kakaoPayInfo(pg_token));
+    }
+
 }
