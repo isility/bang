af94ece8a49c56efd63e471aa9763c04496afb49
PTM <victor.parktaemin@gmail.com>
2023-07-25 16:11:15 +0900

    community_qna commit

--- src/main/java/kr/co/jhta/bang/finalproject/control/ProductController.java
+++ src/main/java/kr/co/jhta/bang/finalproject/control/ProductController.java
@@ -1,6 +1,5 @@
-package kr.co.jhta.bang.finalproject.Control;
+package kr.co.jhta.bang.finalproject.control;
 
-import kr.co.jhta.bang.finalproject.dto.ProductDTO;
 import kr.co.jhta.bang.finalproject.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @Slf4j
--- src/main/java/kr/co/jhta/bang/finalproject/control/QnaController.java
+++ src/main/java/kr/co/jhta/bang/finalproject/control/QnaController.java
@@ -1,4 +1,4 @@
-package kr.co.jhta.bang.finalproject.Control;
+package kr.co.jhta.bang.finalproject.control;
 
 import kr.co.jhta.bang.finalproject.service.QnaService;
 import org.springframework.beans.factory.annotation.Autowired;
