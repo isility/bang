5193a320169177271fbcb35e93679034982f748b
price7 <120269016+price7@users.noreply.github.com>
2023-08-01 19:19:36 +0900

    Update SecurityConfig.java

--- src/main/java/kr/co/jhta/bang/finalproject/config/SecurityConfig.java
+++ src/main/java/kr/co/jhta/bang/finalproject/config/SecurityConfig.java
@@ -16,10 +16,61 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     DataSource dataSource;
 
     @Autowired
-    MemberOAuth2UserDetailService userDetailService;
-
-    @Autowired
     PasswordEncoder passwordEncoder;
+
+
+
+    // 인증 메서드
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String usernameQuery = "SELECT member_id as username, member_pw as password FROM member WHERE member_id = ? ";
+
+        StringBuffer authQuery = new StringBuffer();
+
+        authQuery.append("SELECT m.member_id as username, r.role_name as authority ");
+        authQuery.append("FROM member m, role r ");
+        authQuery.append("WHERE m.role_number = r.role_number and m.role_number = ? ");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(usernameQuery)
+                .authoritiesByUsernameQuery(authQuery.toString())
+                .passwordEncoder(passwordEncoder);
+    }
+
+
+
+    // 정적 리소스의 요청을 지정하고, ignoring() 메서드를 통해 보안 설정을 무시하도록 설정
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+
+    // 인가 메서드
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll() // 로그인 페이지는 모두 접근 가능하도록 허용
+                .antMatchers("/index").authenticated() // "/index" 페이지는 인증된 사용자만 접근 가능
+                .antMatchers("/seller-page").hasRole("SELLER") // "/seller-page" 페이지는 "SELLER" 역할이 있는 사용자만 접근 가능
+                .antMatchers("/**").hasRole("USER") // 기타 모든 페이지는 "USER" 역할이 있는 사용자만 접근 가능
+
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("member_id")
+                .passwordParameter("member_pw")
+                .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/index")
+                    .failureUrl("/login?error=true")
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/index");
+    }
+}
 
 
 
