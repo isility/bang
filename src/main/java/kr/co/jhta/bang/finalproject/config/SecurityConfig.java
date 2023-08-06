0458e99c5db46062bc0aa78692e89db9ae6f90bd
price7 <funfun2110@gmail.com>
2023-08-06 17:06:26 +0900

    JWT token 작업 취소

--- src/main/java/kr/co/jhta/bang/finalproject/config/SecurityConfig.java
+++ src/main/java/kr/co/jhta/bang/finalproject/config/SecurityConfig.java
@@ -17,6 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         super.configure(auth);
     }
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
