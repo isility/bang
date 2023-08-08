193e72ad898ed9eedb860a2e88853b93c85a3b81
gwan9 <rhksgh365@gmail.com>
2023-08-08 09:29:13 +0900

    결제관리 페이지 완료

--- src/main/java/kr/co/jhta/bang/finalproject/control/AdminChartController.java
+++ src/main/java/kr/co/jhta/bang/finalproject/control/AdminChartController.java
@@ -1,15 +1,10 @@
 package kr.co.jhta.bang.finalproject.control;
 
-import kr.co.jhta.bang.finalproject.service.PaymentDetailService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @Slf4j
