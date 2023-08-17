c794c81d0c99ecc3340e4f6100709c8095cd4cf5
gwan9 <rhksgh365@gmail.com>
2023-08-17 11:34:58 +0900

    admin page로 넘어가는 링크 추가

--- src/main/java/kr/co/jhta/bang/finalproject/dao/ReviewDAO.java
+++ src/main/java/kr/co/jhta/bang/finalproject/dao/ReviewDAO.java
@@ -10,42 +10,18 @@ import java.util.List;
 @Repository
 @Mapper
 public interface ReviewDAO {
-
-
     List<ReviewDTO> getAll(); // reply_step = 0인 리뷰들 가져오는 select
     List<ReviewDTO> findAllReply(StartEnd se); // 페이징
     ReviewDTO findByReply_number(int replyNumber); // replyNumber로 reviewDetail로 연결하는 select
     int getTotal();
-
     void writeReply(ReviewDTO dto); // 리뷰 작성하는 insert
-
     void deleteReview(int replyNumber);
     void modifyReview(ReviewDTO dto);
-
-
-    int getReplyRefOrderByReplyNumber(int replyNumber);
-
-    int getReplyRefByReplyNumber(int replyNumber);
-
-
     void setReplyFirst(ReviewDTO dto); // 최초 댓글 작성
     List<ReviewDTO> findAllByReplyRef(int replyNumber); // 댓글들만 가져옴
-    int getReplyAnswerNumberByReplyNumber(int replyNumber); // 최초 댓글인지 확인
-    void setReplyFirstStep(ReviewDTO reviewDTO); // step = 1인 답글
-
-    void updateAnswerNumber(int replyNumber);
-
-    int findNvlRef(int replyNumber);
-    int getReplyNumber(int replyNumber);
-
-  //  ReviewDTO getProductDetail(ReviewDTO reviewDTO);
-
     List<ReviewDTO> getOneByMemberId(String member_id);
-
     String getProductNumberByName(String member_id);
-
     String getProductName(int productNumber);
-
     List<ReviewDTO> getReplyByProductNumber(int productNumber);
 
 }
--- src/main/java/kr/co/jhta/bang/finalproject/service/ReviewService.java
+++ src/main/java/kr/co/jhta/bang/finalproject/service/ReviewService.java
@@ -22,32 +22,21 @@ public class ReviewService {
     }
     // 모든 리뷰글 조회
     public List<ReviewDTO> getAll() { return dao.getAll(); }
-
     // 리뷰번호로 리뷰 하나 조회
     public ReviewDTO findByReply_number(int replyNumber) {
         return dao.findByReply_number(replyNumber);
     }
-
-
     public int getTotal(){
         return dao.getTotal();
     }
-
-
-    // review 작성 로직
     public int writeReply(ReviewDTO dto){
         dao.writeReply(dto);
         return dto.getReplyNumber();
     }
-
-    // review 삭제
     public void deleteReview(int replyNumber){
         dao.deleteReview(replyNumber);
     }
-
     public void modifyReview(ReviewDTO reviewDTO)  {dao.modifyReview(reviewDTO);}
-
-
     public List<ReviewDTO> firstCommentsSave(ReviewDTO reviewDTO) {
         log.info("reviewDTO: {}", reviewDTO);
         dao.setReplyFirst(reviewDTO);
@@ -56,25 +45,19 @@ public class ReviewService {
 
         return savedReviews;
     }
-
     public List<ReviewDTO> findAllByReplyRef(int refNumber){
         return dao.findAllByReplyRef(refNumber);
     }
-
     public List<ReviewDTO> getOneByMemberId(String member_id){
         return dao.getOneByMemberId(member_id);
     }
-
     public String getProductNumberByName(String member_id){
         return dao.getProductNumberByName(member_id);
     }
-
     public String getProductName(int productNumber){
         return dao.getProductName(productNumber);
     }
-
     public List<ReviewDTO> getReplyByProductNumber(int productNumber){
-
         return dao.getReplyByProductNumber(productNumber);
     }
 }
