6dcbd11c9402104d5c14a7cf871b297b43dd80d9
PTM <victor.parktaemin@gmail.com>
2023-08-17 12:23:59 +0900

    notice&qna commit

--- src/main/java/kr/co/jhta/bang/finalproject/control/NoticeController.java
+++ src/main/java/kr/co/jhta/bang/finalproject/control/NoticeController.java
@@ -1,9 +1,12 @@
 package kr.co.jhta.bang.finalproject.control;
 
+import kr.co.jhta.bang.finalproject.dto.CartDTO;
 import kr.co.jhta.bang.finalproject.dto.NoticeDTO;
 import kr.co.jhta.bang.finalproject.dto.QnaDTO;
 import kr.co.jhta.bang.finalproject.service.NoticeService;
+import kr.co.jhta.bang.finalproject.service.PaymentService;
 import kr.co.jhta.bang.finalproject.util.PageUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +15,16 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.Map;
-
+@Slf4j
 @Controller
 @RequestMapping("/")
 public class NoticeController {
 
     @Autowired
     NoticeService service;
+
+    @Autowired
+    PaymentService payService;
 
     @GetMapping("notice/noticeList")
     public String list(Model model, Principal principal, @RequestParam(name = "currentPage", defaultValue = "1")int currentPage){
@@ -32,7 +38,21 @@ public class NoticeController {
         int endNo = (int)map.get("endNo");
         model.addAttribute("list", service.selectAll(startNo, endNo));
         model.addAttribute("map", map);
-        model.addAttribute("username", principal.getName());
+
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
 
         return "notice/noticeList";
     }
@@ -40,27 +60,73 @@ public class NoticeController {
     @GetMapping("notice/noticeDetail")
     public String form(@RequestParam("noticeNumber")int noticeNumber, Model model, Principal principal){
         model.addAttribute("dto", service.selectOne(noticeNumber));
-        model.addAttribute("username", principal.getName());
+
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
 
         return "notice/noticeDetail";
     }
 
-    @GetMapping("notice/noticeWrite")
+    @GetMapping("admin/notice/noticeWrite")
     public String writeForm(Model model, Principal principal){
-        model.addAttribute("username", principal.getName());
+
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
+
         return "notice/noticeWrite";
     }
 
-    @PostMapping("notice/noticeWrite")
+    @PostMapping("admin/notice/noticeWrite")
     public String writeOk(@ModelAttribute NoticeDTO dto, HttpServletRequest req){
+
         service.addOne(dto);
+
         return "redirect:/notice/noticeList";
     }
 
     @GetMapping("notice/noticeModify")
     public String modifyForm(@RequestParam("noticeNumber")int noticeNumber, Model model, Principal principal) {
         model.addAttribute("dto", service.selectOne(noticeNumber));
-        model.addAttribute("username", principal.getName());
+
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
+
         return "notice/noticeModify";
     }
 
--- src/main/java/kr/co/jhta/bang/finalproject/control/QnaController.java
+++ src/main/java/kr/co/jhta/bang/finalproject/control/QnaController.java
@@ -1,8 +1,11 @@
 package kr.co.jhta.bang.finalproject.control;
 
+import kr.co.jhta.bang.finalproject.dto.CartDTO;
 import kr.co.jhta.bang.finalproject.dto.QnaDTO;
+import kr.co.jhta.bang.finalproject.service.PaymentService;
 import kr.co.jhta.bang.finalproject.service.QnaService;
 import kr.co.jhta.bang.finalproject.util.PageUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +17,21 @@ import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 import java.util.List;
 import java.util.Map;
-
+@Slf4j
 @Controller
 @RequestMapping("/")
 public class QnaController {
     @Autowired
     QnaService service;
 
+    @Autowired
+    PaymentService payService;
+
     @GetMapping("qna/getQnaTypeValues")
     public ResponseEntity<List<QnaDTO>> getQnaTypeValues(@RequestParam String value) {
         List<QnaDTO> qnaList = service.getQnaListByType(value); // 선택한 타입에 해당하는 Q&A 리스트 조회
+
+
         return new ResponseEntity<>(qnaList, HttpStatus.OK);
     }
 
@@ -70,7 +78,21 @@ public class QnaController {
         model.addAttribute("list", qnaList);
         model.addAttribute("map", map);
         model.addAttribute("qnaType", qnaType); // qnaType을 모델에 추가하여 View에서 사용할 수 있도록 함
-        model.addAttribute("username", principal.getName());
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
+
         return "qna/qnaList";
     }
 
@@ -81,7 +103,22 @@ public class QnaController {
     @GetMapping("qna/qnaDetail")
     public String form(@RequestParam("qnaNumber")int qnaNumber, Model model, Principal principal){
         model.addAttribute("dto", service.selectOne(qnaNumber));
-        model.addAttribute("username", principal.getName());
+
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
+
         return "qna/qnaDetail";
     }
 
@@ -90,25 +127,55 @@ public class QnaController {
 
     @GetMapping("qna/qnaWrite")
     public String writeForm(Model model, Principal principal){
-        model.addAttribute("username", principal.getName());
+
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
+
         return "qna/qnaWrite";
     }
 
     @PostMapping("qna/qnaWrite")
-    public String writeOk(@ModelAttribute QnaDTO dto, HttpServletRequest req){
+    public String writeOk(@ModelAttribute QnaDTO dto, HttpServletRequest req, Principal principal){
         service.addOne(dto);
         return "redirect:/qna/qnaList";
     }
 
     @GetMapping("qna/qnaModify")
     public String modifyForm(@RequestParam("qnaNumber")int qnaNumber, Model model, Principal principal) {
-        model.addAttribute("username", principal.getName());
         model.addAttribute("dto", service.selectOne(qnaNumber));
+
+        if (principal != null) {
+            int cnt = 0;
+            log.info("로그인된 사용자");
+            log.info("Authentication 의 반환객체 : {}", principal.getName());
+            for (CartDTO dto : payService.cartlist(principal.getName()))
+                cnt++;
+            model.addAttribute("cartListCount", cnt);
+            model.addAttribute("username", principal.getName());
+        } else {
+            log.info("로그인하지 않은 사용자");
+            model.addAttribute("username", "Guest 님"); // 로그인하지 않은 사용자는 "Guest"라는 이름으로 보내기
+            model.addAttribute("cartListCount", 0);
+        }
+
         return "qna/qnaModify";
     }
 
     @PostMapping("qna/qnaModify")
-    public String modifyOk(@ModelAttribute QnaDTO dto) {
+    public String modifyOk(@ModelAttribute QnaDTO dto, Principal principal) {
+
         service.modifyOne(dto);
         return "redirect:/qna/qnaList";
     }
